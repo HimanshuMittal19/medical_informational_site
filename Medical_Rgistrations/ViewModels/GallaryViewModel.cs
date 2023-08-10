a38ea72776b8edfcf0e7ade3b947b8eb0bccdcff
HimanshuMittal19 <141625040+HimanshuMittal19@users.noreply.github.com>
2023-08-10 14:01:19 +0530

    Shiv Changes
    Shiv Changes

---
+++ Medical_Rgistrations/ViewModels/GallaryViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Medical_Rgistrations.ViewModels
+{
+    
+
+    public class GallaryViewModel
+    {
+        public GallaryViewModel()
+        {
+            this.ImageGroups = new List<string>();
+        }
+
+        
+        [Display(Name ="Group Name")]
+        public string groupName { get; set; }
+        [Display(Name = "Gallery Template Name")]
+        public string GalleryName { get; set; }
+
+        [Display(Name = "Images")]
+        public List<IFormFile> imageFiles { get; set; }
+        [Display(Name = "Active")]
+        public bool active { get; set; }
+
+        public List<string> ImageGroups { get; set; }
+
+    }
+
+}
